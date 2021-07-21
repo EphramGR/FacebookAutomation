b7c77460999dcc88815c7f5be4f6c2e419d1d41f
EphramGR <86477964+EphramGR@users.noreply.github.com>
2021-07-21 13:43:54 -0400

    Add files via upload

--- facebookGetAd.py
+++ facebookGetAd.py
@@ -26,7 +26,7 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='Deleting an ad off Facebook')
-parser.add_argument('--addir',type=str, help='Diectory of the folder with yhe .yaml file for your login info, and the dump.yaml file.')
+parser.add_argument('--addir',type=str, help='Diectory of the folder with the .yaml file for your login info, and the dump.yaml file.')
 args = parser.parse_args()
 
 fileDir = args.addir
@@ -67,41 +67,148 @@ def gotoMarketplace():
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Your Account"))
     )
-    element.click() 
-    
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".bi6gxh9e > .oajrlxb2 > .rq0escxv"))
-    )
-    element.click() 
+    element.click()  
 
   except:
     print("oops")
 
 def getAds():
-  time.sleep(1)
-  x = True
-  y = 1
-  while x == True:
-    try:
-      element = WebDriverWait(driver, 5).until(
-          EC.presence_of_element_located((By.XPATH, "//div[3]/div/span/div/div[y]/div"))
-      )
-      element.click()
-      pyautogui.press('escape')
-    except:
-      x = False
-      print("TestFailed")
+
+  try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//div[2]/div/div[2]/div/div[2]/div/div[4]/div/div/div")) 
+    )
+    element.click()
+  except:
+    print("oops2")
+    
+  #driver.find_element(By.XPATH, "//div[2]/div/div[2]/div/div[2]/div/div[4]/div/div/div").click() #ad one
+  #driver.find_element(By.XPATH, "//div[3]/div/div[2]/div/div[2]/div/div[4]/div/div/div").click() #ad two
+  #driver.find_element(By.XPATH, "//div[x]/div/div[2]/div/div[2]/div/div[4]/div/div/div").click() #ad swap
+  
+  try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Edit Listing")) 
+    )
+    element.click()
+  except:
+    print("oops3")
+    
+  try:
+    title = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//div/input")) 
+    )
+  except:
+    print("oops4")
+  
+  titleValue = title.get_attribute("value"); 
+  print(titleValue)
+
+  try:
+    price = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//div[5]/div/div/label/div/div/input")) 
+    )
+  except:
+    print("oops5")
+    
+  priceValue = price.get_attribute("value"); 
+  print(priceValue)
+  
+  try:
+    desc = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//textarea")) 
+    )
+  except:
+    print("oops6")
+
+  descValue = desc.get_attribute("value");
+  print(descValue)
+
+  try:
+    condition = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//div[7]/div/div/div/label/div/div/div/div/span")) 
+    )
+  except:
+    print("oops7")
+
+  conditionValue = condition.get_attribute("innerText");
+  print(conditionValue)
+
+  try:
+    category = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//label/div/div/div/div/span")) 
+    )
+  except:
+    print("oops8")
+
+  categoryValue = category.get_attribute("innerText");
+  print(categoryValue)
+
+  imageValue = ['','','','','','','','','','']
+  for x in range(10):
+    if x == 0:
+      try:
+        image = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//div[3]/div[2]/div/div/div/div/div/img")) 
+        )
+      except:
+        print("oops9 ", x)
+
+    imagePathBroke = "//div[", str(x + 1), "]/div/div/div/img"
+    imagePathJoin = ''.join(imagePathBroke)
+
+    if x > 0:
+      try:
+        image = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, imagePathJoin)) 
+        )
+      except:
+        break   
+    
+
+    imageValue[x] = image.get_attribute("src");
+  print(imageValue)
+
+  y = True
+  x = 0
+  tagValue = []
+  while y == True:
+    x += 1
+    if x == 1:
+      try:
+        tag = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//label/div/div/div/div/div/div/div/span/span")) 
+        )
+      except:
+        print("oops10 ", x)
+
+    tagPathBroke = "//div[", str(x), "]/div/span/span"
+    tagPathJoin = ''.join(tagPathBroke)
+
+    if x > 1:
+      try:
+        tag = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, tagPathJoin)) 
+        )
+      except:
+        break   
+        
+    tagValue.append(tag.get_attribute("innerText"))
       
-    y += 1
-
-
-#After manage my listings:
-#//div[3]/div/span/div/div[x+1]/div
+  print(tagValue)
+  
+  try:
+    stock = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//span/div/div/div/div/label/div/div/div/div/span")) 
+    )
+  except:
+    print("oops11")
+    
+  stockValue = stock.get_attribute("innerText"); 
+  print(stockValue)
 
 login()
 gotoMarketplace()
 getAds()
 
-#while loop try test for all of them div[0 + 1 each time]
-#grabs info
-#if error except: outta while loop
+
